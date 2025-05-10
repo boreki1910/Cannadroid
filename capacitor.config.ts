dc2db068e43c5912380e26e4b2493df8f8014260
boreki1910 <stefanschnitzler1910@gmail.com>
2025-05-10 22:26:53 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.growtracker',
+  appName: 'Cannabis Grow Tracker',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
