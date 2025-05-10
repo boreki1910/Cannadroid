c376d14841fdbb63fdebc5a0b886c6e8c908007d
boreki1910 <stefanschnitzler1910@gmail.com>
2025-05-10 22:16:39 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Home } from './pages/Home';
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Cannabis Grow Tracker</h1>
+      <Home />
+    </div>
+  );
+};
